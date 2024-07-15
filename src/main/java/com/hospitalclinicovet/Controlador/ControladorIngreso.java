384cf6ae85261dd1816ef41166249b7a668d0de7
anpeher <anpeher12@gmail.com>
2024-07-16 00:35:20 +0200

    implementacion del swagger y endpoint documentados correctamente

--- src/main/java/com/hospitalclinicovet/Controlador/ControladorIngreso.java
+++ src/main/java/com/hospitalclinicovet/Controlador/ControladorIngreso.java
@@ -6,6 +6,12 @@ import com.hospitalclinicovet.dto.Ingreso.NuevoIngresoDTO;
 import com.hospitalclinicovet.dto.Respuesta.Message;
 import com.hospitalclinicovet.Modelo.Ingreso.Ingreso;
 import com.hospitalclinicovet.Servicio.Ingreso.ServicioIngreso;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,14 +25,22 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/ingreso")
 @AllArgsConstructor
+@Tag(name = "Controlador de Ingresos", description = "Endpoints para la creación y manejo de ingresos de una mascota en un hospital veterinario")
 public class ControladorIngreso {
 
     private final ServicioIngreso servicioIngreso;
 
-
+    @Operation(summary = "Generacion de un nuevo ingreso",
+            description = "Genera un nuevo ingreso para una mascota existente y activa. Esta se crea con el estado de alta sin fecha de finalización y solo puede ser creada por el responsable de la mascota.",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Ingreso creado exitosamente",
+                            content = @Content(schema = @Schema(implementation = Ingreso.class))),
+                    @ApiResponse(responseCode = "400", description = "Solicitud mal escrita o la mascota no está activa",
+                            content = @Content(schema = @Schema(implementation = Message.class)))
+            })
     @PostMapping
     public ResponseEntity generarIngreso(@Valid @RequestBody NuevoIngresoDTO ingresoDTO, BindingResult bindingResult){
-        if (bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) { //se validan los datos del json y en caso de fallo salta con un bad request
             return new ResponseEntity<>(new Message(Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage()), HttpStatus.BAD_REQUEST);
         }
         try{
@@ -37,7 +51,18 @@ public class ControladorIngreso {
 
     }
 
+    @Operation(summary = "Modifica un ingreso",
+            description = "Modifica un ingreso existente cambiando su fecha de finalización y/o estado. No puede tener estado FINALIZADO sin tener una fecha de finalización",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Ingreso modificado exitosamente",
+                            content = @Content(schema = @Schema(implementation = Ingreso.class))),
+                    @ApiResponse(responseCode = "400", description = "Solicitud mal escrita",
+                            content = @Content(schema = @Schema(implementation = Message.class))),
+                    @ApiResponse(responseCode = "404", description = "No existe el ingreso",
+                            content = @Content(schema = @Schema(implementation = Message.class)))
+            })
     @PutMapping("/{idIngreso}")
+    //No se es necesario el id de la mascota y por tanto se omite
     public ResponseEntity modificarIngreso(@PathVariable("idIngreso") Long id,@Valid @RequestBody ModIngresoDTO modIngresoDTO, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return new ResponseEntity<>(new Message(Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage()), HttpStatus.BAD_REQUEST);
@@ -52,11 +77,25 @@ public class ControladorIngreso {
         }
     }
 
+    @Operation(summary = "Muestra una lista de todos los ingresos",
+            description = "Muestra una lista de todos los ingresos.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Lista generada exitosamente",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = Ingreso.class))))
+            })
     @GetMapping
     public ResponseEntity listarIngresos(){
         return new ResponseEntity<>(servicioIngreso.listaIngresos(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Modifica el estado de un ingreso ha ANULADO",
+            description = "Elimina un ingreso cambiado su estado ha ANULADO.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "estado de ingreso modificado ha ANULADO exitosamente",
+                            content = @Content()),
+                    @ApiResponse(responseCode = "404", description = "No existe el ingreso",
+                            content = @Content(schema = @Schema(implementation = Message.class)))
+            })
     @DeleteMapping("/{idIngreso}")
     public ResponseEntity eliminarIngreso(@PathVariable("idIngreso") Long id){
         try{
--- src/main/java/com/hospitalclinicovet/Controlador/ControladorMascota.java
+++ src/main/java/com/hospitalclinicovet/Controlador/ControladorMascota.java
@@ -7,6 +7,12 @@ import com.hospitalclinicovet.dto.Respuesta.Message;
 import com.hospitalclinicovet.Modelo.Ingreso.Ingreso;
 import com.hospitalclinicovet.Modelo.Mascota.Mascota;
 import com.hospitalclinicovet.Servicio.Mascota.MascotaServicio;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,13 +26,23 @@ import java.util.Objects;
 @RestController
 @RequestMapping("/mascota")
 @AllArgsConstructor
+@Tag(name = "Controlador de mascotas", description = "Endpoints para guardar la información sobre una mascota registrada en el hospital clínico")
 public class ControladorMascota {
 
     private final MascotaServicio mascotaServicio;
 
+
+    @Operation(summary = "Registro de una nueva mascota",
+            description = "Se registra una mascota en el hospital guardando sus datos personales. Su código de identificación debe ser único y por defecto la mascota está activa.",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Mascota registrada correctamente",
+                            content = @Content(schema = @Schema(implementation = Mascota.class))),
+                    @ApiResponse(responseCode = "400", description = "Solicitud mal escrita",
+                            content = @Content(schema = @Schema(implementation = Message.class)))
+            })
     @PostMapping
     public ResponseEntity guardarMascota(@Valid @RequestBody MascotaDTO mascotaDTO, BindingResult bindingResult){
-        if (bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) { //se validan los datos del json y en caso de fallo salta con un bad request
             return new ResponseEntity<>(new Message(Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage()), HttpStatus.BAD_REQUEST);
         }
         try {
@@ -37,6 +53,16 @@ public class ControladorMascota {
         }
     }
 
+    @Operation(summary = "Obtener los datos de una mascota",
+            description = "A traves del id de la mascota obtenemos su información. Si no está activa como pone el enunciado no se devolverá la información, pero se tratará como un error de BADREQUEST por si en un futuro se decide que si se puede acceder a esa información. Esto lo hacemos con todos los endpoints",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Mascota obtenida correctamente",
+                            content = @Content(schema = @Schema(implementation = Mascota.class))),
+                    @ApiResponse(responseCode = "400", description = "Solicitud mal escrita o la mascota no está activa",
+                            content = @Content(schema = @Schema(implementation = Message.class))),
+                    @ApiResponse(responseCode = "404", description = "No existe la mascota",
+                            content = @Content(schema = @Schema(implementation = Message.class)))
+            })
     @GetMapping("/{idMascota}")
     public ResponseEntity obtenerMascota(@PathVariable("idMascota") Long id){
         try {
@@ -48,6 +74,18 @@ public class ControladorMascota {
         }
     }
 
+    @Operation(summary = "Obtener todos los ingresos de una mascota",
+            description = "A traves del id de la mascota obtenemos todas los registro de ingreso de una mascota.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Ingresos de la mascota obtenidos correctamente",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = Ingreso.class)))),
+                    @ApiResponse(responseCode = "204", description = "Ingresos de la mascota obtenidos correctamente, pero no tiene ninguno",
+                            content = @Content()),
+                    @ApiResponse(responseCode = "400", description = "Solicitud mal escrita o la mascota no está activa",
+                            content = @Content(schema = @Schema(implementation = Message.class))),
+                    @ApiResponse(responseCode = "404", description = "No existe la mascota",
+                            content = @Content(schema = @Schema(implementation = Message.class)))
+            })
     @GetMapping("/{idIngreso}/ingreso")
     public ResponseEntity ingresosMascota(@PathVariable("idIngreso") Long id){
         try {
@@ -63,6 +101,16 @@ public class ControladorMascota {
         }
     }
 
+    @Operation(summary = "Se hacen inaccesibles los datos de una mascota",
+            description = "A traves del id de la mascota cambiamos la variable activa a false haciendo que no se puedan realizar operacion con ellas pero manteniéndola en la memoria.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "variable activa cambiada a false",
+                            content = @Content()),
+                    @ApiResponse(responseCode = "400", description = "La mascota no está activa",
+                            content = @Content(schema = @Schema(implementation = Message.class))),
+                    @ApiResponse(responseCode = "404", description = "No existe la mascota",
+                            content = @Content(schema = @Schema(implementation = Message.class)))
+            })
     @DeleteMapping("/{idMascota}")
     public ResponseEntity eliminarMascota(@PathVariable("idMascota") Long id){
         try {
